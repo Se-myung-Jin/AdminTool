e09f54fe40cb0ad8706b6624816d051c184b0c14
Se-myung-Jin <tpaud94@naver.com>
2021-01-18 23:54:40 +0900

    운영툴 프로젝트 생성

---
+++ AdminTool/AdminTool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
